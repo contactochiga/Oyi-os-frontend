f83901d22fa926d61ab28905fc86e1861d3b4925
contactochiga <contactochiga@gmail.com>
2025-12-10 09:56:12 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  },
-  images: { unoptimized: true }
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
