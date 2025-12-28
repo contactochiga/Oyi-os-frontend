3e9f9899924f1766e983ae6c26b280cbb4d4c5b6
contactochiga <contactochiga@gmail.com>
2025-12-28 13:40:12 +0100

    Update RemotePanelRenderer.tsx

--- src/app/components/remotes/RemotePanelRenderer.tsx
+++ src/app/components/remotes/RemotePanelRenderer.tsx
@@ -1,23 +1,27 @@
 import LightPanel from "./LightPanel";
-// later: import AcPanel, TvPanel, etc.
 
 export default function RemotePanelRenderer({
   panel,
   deviceId,
+  lastUpdated,
+  onInteraction,
 }: {
   panel?: string | null;
   deviceId?: string;
+  lastUpdated: number;
+  onInteraction: () => void;
 }) {
   if (!panel) return null;
 
   switch (panel) {
     case "light":
-      return <LightPanel deviceId={deviceId} />;
-
-    // case "ac":
-    // case "tv":
-    // case "door":
-    // case "cctv":
+      return (
+        <LightPanel
+          deviceId={deviceId}
+          lastUpdated={lastUpdated}
+          onInteraction={onInteraction}
+        />
+      );
 
     default:
       return null;
